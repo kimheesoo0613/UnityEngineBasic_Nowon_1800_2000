666ab3754bf75b9be4ca087601c7687a047d5da9
kimheesoo0613 <113429487+kimheesoo0613@users.noreply.github.com>
2022-09-15 19:52:31 +0900

    ClassObjectlnstance
    ClassObjectlnstance

---
+++ CSharp/Firstproject/ClassObjectlnstance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassObjectlnstance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Human
+    {
+        int ago;
+        float height;
+        double weight;
+        bool isReseting;       
+        char genderCharacter;        
+        string name;
+
+        void Breath()
+        {
+            Console.WriteLine(name + "(이)가 숨을 쉰다")
+            Console.WriteLine($"{name})
+        }
+    }
+}
---
+++ CSharp/Firstproject/Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        // 함수 정의
+        // 반환타입 함수이름(매개변수1탑입 매개변수1이름, 매개변수2타입 매개변수2이름 ...)
+        // {
+        //     ...
+        //     연산내용
+        //     ...
+        //     retuen 반환값;
+        // }
+        //
+        // void 반환형 함수에서는 함수 마지막에 return 을 생략할 수 있다.
+        static void Main(string[] args)
+        {
+            // 함수 호출 형태
+            // 함수이름(인자1, 인자2 ...);
+            PrintHelloWorld();
+            PrintNum(3);
+            int result = 0;
+            result = Sum(1, 2);
+            PrintNum(result);
+        }
+
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void PrintNum(int num)
+        {
+            Console.WriteLine(num);
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
