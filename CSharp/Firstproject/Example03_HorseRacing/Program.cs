50bf89703bc9e46be84a51961d19e386577afe11
kimheesoo0613 <113429487+kimheesoo0613@users.noreply.github.com>
2022-09-27 19:47:12 +0900

    Horse
    Horse *5

---
+++ CSharp/Firstproject/Example03_HorseRacing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example03_HorseRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for Holse
+            
+        }
+    }
+    public class Horse
+    {
+
+    }
+}
