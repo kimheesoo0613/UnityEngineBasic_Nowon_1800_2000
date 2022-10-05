74b60be7be3173075a3dbbe92cf614c98e129b72
kimheesoo <kimheesoo0613@gmail.com>
2022-10-05 20:21:18 +0900

    IEnumerator, GetEnumerator
    IEnumerator, GetEnumerator

--- CSharp/Firstproject/MyDynamicArrray/MyDinamicArray.cs
+++ CSharp/Firstproject/MyDynamicArrray/MyDinamicArray.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.SymbolStore;
 using System.Linq;
@@ -7,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace MyDynamicArrray
 {
-    internal class MyDinamicArray
+    public class MyDinamicArray<T> : IEnumerable<T>
     {
         private const int DEFAULT_SIZE = 1;
-        private int[] _data = new int[DEFAULT_SIZE];
-        private int this[int index]
+        private T[] _data = new T[DEFAULT_SIZE];
+        public T this[int index]
         {
             get
             {
@@ -24,16 +25,15 @@ namespace MyDynamicArrray
             }
         }
         public int Count;
-        public int Capacity;
-
-        public void Add(int item)
+        public int Capacity => _data.Length; // get 접근자의 람다식 표현, 람다식 : 익명함수 표현방법
+         
+        public void Add(T item)
         {
             // 배열의 크기가 모자라면
             if (Count >= Capacity)
             {
-                Capacity *= 2;
                 // 2배짜리 새로운 배열 생성
-                int[] tmp = new int[Capacity];
+                T[] tmp = new T[Capacity * 2];
 
                 // 기존 데이터를 새로운 배열에 복제
                 for (int i = 0; i < Count; i++)
@@ -46,7 +46,12 @@ namespace MyDynamicArrray
             _data[Count] = item;
             Count++;
         }
-
+         /// <summary>
+         /// 특정 인덱스에 있는 데이터를 지우고
+         /// 그 뒤 모든 인덱스 데이터들을 한칸씩 앞으로 당겨줌
+         /// </summary>
+         /// <param name="index">지우고 싶은 데이터의 인덱스</param>
+         /// <returns>true : 삭제됨 , false : 삭제안됨 </returns>
         public bool RemoveAt(int index)
         {
             if (index < 0 || index > Count - 1)
@@ -57,60 +62,111 @@ namespace MyDynamicArrray
                 _data[i] = _data[i + 1];
             }
             Count--;
-            _data[Count] = 1;
+            _data[Count] = default(T);
             return true;
         }
 
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Comparer<T>.Default.Compare(_data[i], item) == 0)
+                    return RemoveAt(i);
+            }
+            return false;
+        }
 
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MyDynamicArratEnum<T>(_data, this);
+        }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyDynamicArratEnum<T>(_data, this);
+        }
 
+    }
+    public class MyDynamicArratEnum<T> : IEnumerator<T>
+    {
+        private readonly T[] _data;
+        private readonly MyDinamicArray<T> _dynamicArray;
+        private int _index = -1;
+        private int _count => _dynamicArray.Count;
+        public T Current
+        {
+            get
+            {
+                return _data[_index];
+            }
+        }
 
+        object IEnumerator.Current => Current;
 
+        public MyDynamicArratEnum(T[] data, MyDinamicArray<T> dynamicArray)
+        {
+            _data = data;
+            _dynamicArray = dynamicArray;
+        }
 
+        public void Dispose()
+        {
+            // nothing to do
+        }
 
+        public bool MoveNext()
+        {
+            _index++;
+            return _index >= 0 && _index < _count;
+        }
 
-
-
-
-
-        // private int _hp;
-        // 
-        // public int Hp
-        // {
-        //     get
-        //     {
-        //         return _hp;
-        //     }
-        // 
-        //     private set
-        //     {
-        //         if (value < 0)
-        //             value = 0;
-        // 
-        //         _hp = value;
-        //         _hpBar.value = _hp / _hpMax;
-        //     }
-        // }
-        // 
-        // public int Mp { get; private set; }
-        // 
-        // private UISLider _hpBar;
-        // private int _hpMax;
-        // 
-        // public int GetHP()
-        // {
-        //     return _hp;
-        // }
-        // 
-        // public void SetHp(int value)
-        // {
-        //     if (value < 0)
-        //         value = 0;
-        // 
-        //     _hp = value;
-        //     _hpBar.value = _hp / _hpMax;
-        // }
-        // 
-        // class UISLider { public float value; }
+        public void Reset()
+        {
+            _index = -1;
+        }
     }
 }
+
+
+
+    // 프로퍼티 2
+
+// private int _hp;
+// 
+// public int Hp
+// {
+//     get
+//     {
+//         return _hp;
+//     }
+// 
+//     private set
+//     {
+//         if (value < 0)
+//             value = 0;
+// 
+//         _hp = value;
+//         _hpBar.value = _hp / _hpMax;
+//     }
+// }
+// 
+// public int Mp { get; private set; }
+// 
+// private UISLider _hpBar;
+// private int _hpMax;
+// 
+// public int GetHP()
+// {
+//     return _hp;
+// }
+// 
+// public void SetHp(int value)
+// {
+//     if (value < 0)
+//         value = 0;
+// 
+//     _hp = value;
+//     _hpBar.value = _hp / _hpMax;
+// }
+// 
+// class UISLider { public float value; }
--- CSharp/Firstproject/MyDynamicArrray/Program.cs
+++ CSharp/Firstproject/MyDynamicArrray/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace MyDynamicArrray
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            Console.WriteLine(list[0]);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            // foreach 문 구현 원리
+            using IEnumerator<int> enumerator = list.GetEnumerator();
+            {
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current);
+                }
+            }
+
+            MyDinamicArray<int> dynamicArray = new MyDinamicArray<int>(); 
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Add(4);
+            dynamicArray.Remove(1);
+            Console.WriteLine(dynamicArray[0]);
+
+            for (int i = 0; i < dynamicArray.Count; i++)
+            {
+                Console.WriteLine(dynamicArray[i]);
+            }
+
+            foreach (int item in dynamicArray)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
