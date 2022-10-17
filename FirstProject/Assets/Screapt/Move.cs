482b81af3e214381195a0ff9ea92876641f55896
kimheesoo <kimheesoo0613@gmail.com>
2022-10-17 19:49:23 +0900

    FirstProject Move/Test
    FirstProject Move/Test

--- FirstProject/Assets/Screapt/Move.cs
+++ FirstProject/Assets/Screapt/Move.cs
@@ -4,4 +4,20 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+
+    float _h;
+    float _v;
+    private void Update()
+    {
+        _h = Input.GetAxisRaw("Horizontal");
+        _v = Input.GetAxisRaw("Vertical");
+        Debug.Log(_h);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 dir = new Vector3(_h, 0.0f, _v).normalized;
+
+        transform.position += dir * Time.fixedDeltaTime;
+    }
 }
